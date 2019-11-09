bba2446f7d5991de55ca2e7cdebfee74629069ef
梦幻 <web2018_lang@163.com>
2019-11-09 21:39:46 +0800

    随机十六进制颜色

--- kist.js
+++ kist.js
@@ -32,3 +32,28 @@ var kist = function () {
         }
     }
 }
+
+function sixteen() {
+            //随机生成十六进制颜色
+        //#00ff00;
+        var arr = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+        //获取随机数组的索引值0-16
+        function index(n,m){
+            return Math.floor(Math.random()*(m-n+1) +n);
+        }
+        //color:#006655;
+        function Color(){
+            var y = arr[index(0,15)];
+            var e = arr[index(0,15)];
+            var s = arr[index(0,15)];
+            var x = arr[index(0,15)];
+            var w = arr[index(0,15)];
+            var l = arr[index(0,15)];
+            var color = '#'+y + e + s + x + w +l;
+            return color;
+            console.log(color);
+        }
+        var shiColor = Color();
+        console.log(shiColor);
+        document.body.style.backgroundColor = shiColor;
+}
